f8da7c62cdfcf24fe5801b34f226ad5e3a8cfb8b
Irfan Machmud <92133713+irfanEm@users.noreply.github.com>
2022-12-26 21:38:39 +0700

    contoh middleware dan cara meregistrasikan middleware secara global

---
+++ app/Http/Middleware/ContohMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ContohMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $apiKey = $request->header('X-API-Key');
+        if($apiKey == 'Irfan')
+        {
+            return $next($request);
+        }else{
+            return Response("Akses Ditolak", 401);
+        }
+    }
+}
